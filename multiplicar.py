9da094ad449993e7c59884d4165aba62467bffe8
Melisasa2 <118138550+Melisasa2@users.noreply.github.com>
2024-04-04 18:46:19 -0500

    Create multiplicar.py

---
+++ multiplicar.py
@@ -0,0 +1,18 @@
+def multiplicar_matrices(matriz1, matriz2):
+    matriz_rotada = rotar_matriz(matriz2)
+    forma_matriz1 = obtener_forma(matriz1)
+    forma_matriz2 = obtener_forma(matriz2)
+    if forma_matriz1[1] != forma_matriz2[0]:
+        return None
+    producto = []
+    altura_matriz1 = len(matriz1)
+    for fila_matriz1 in range(altura_matriz1):
+        fila_producto = []
+        ancho_matriz_rotada = len(matriz_rotada)
+        for columna_rotada in range(ancho_matriz_rotada):
+            suma = 0
+            for indice_elemento in range(len(matriz1[fila_matriz1])):
+                suma += matriz1[fila_matriz1][indice_elemento] * matriz_rotada[columna_rotada][indice_elemento]
+            fila_producto.append(suma)
+        producto.append(fila_producto)
+    return producto
